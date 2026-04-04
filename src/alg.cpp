70e6b844fe5f8eb9302a883d54e44511dcbb4b64
polinanaumovaa <pnaumova32@gmail.com>
2026-04-05 02:38:30 +0300

    Наумова.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,7 +2,7 @@
 int countPairs1(int *arr, int len, int value) {
   int count = 0;
   for (int i = 0; i < len; i++) {
-    for (int j = i + 1; j < len; J++) {
+    for (int j = i + 1; j < len; j++) {
       if (arr[i] + arr[j] == value) {
         count++;
       }
